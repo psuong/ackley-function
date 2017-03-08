4636f5c11c7dcf675001fae68598653d93b8c451
Allen Kim <allenkim95@gmail.com>
2017-03-08 16:46:22 -0500

    Add plot2d for ackley

--- ackley.py
+++ ackley.py
@@ -9,4 +9,6 @@ def ackley(x, a=20, b=0.2, c=2*np.pi):
     cos_term = -np.exp(sum(np.cos(c*x) / d))
     return a + np.exp(1) + sum_sq_term + cos_term
 
-
+# We want to optimize Ackley for two variables
+print(ackley(np.array([1,2])))
+print(ackley(np.array([0,0])))
--- plot_ackley.py
+++ plot_ackley.py
@@ -3,28 +3,40 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+from ackley import ackley
 
-# Make data.
-X = np.arange(-32, 32, 0.25)
-Y = np.arange(-32, 32, 0.25)
-X, Y = np.meshgrid(X, Y)
+def plot_ackley_3d():
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
 
-a = 20
-b = 0.2
-c = 2 * np.pi
+    # Make data.
+    X = np.arange(-32, 32, 0.25)
+    Y = np.arange(-32, 32, 0.25)
+    X, Y = np.meshgrid(X, Y)
 
-sum_sq_term = -a * np.exp(-b * np.sqrt(X*X + Y*Y) / 2)
-cos_term = -np.exp((np.cos(c*X) + np.cos(c*Y)) / 2)
-Z = a + np.exp(1) + sum_sq_term + cos_term
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
 
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                               linewidth=0, antialiased=False)
+    sum_sq_term = -a * np.exp(-b * np.sqrt(X*X + Y*Y) / 2)
+    cos_term = -np.exp((np.cos(c*X) + np.cos(c*Y)) / 2)
+    Z = a + np.exp(1) + sum_sq_term + cos_term
 
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                                   linewidth=0, antialiased=False)
 
-plt.show()
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
 
+    plt.show()
+
+def plot_ackley_2d():
+    x = np.arange(-32, 33)
+    y = [ackley(np.array([x_i])) for x_i in x]
+    plt.plot(x,y)
+    plt.show()
+
+
+if __name__=="__main__":
+   plot_ackley_2d() 
