5827b57851b59b21fab3e379f367c04d6a4bb5df
Allen Kim <akim003@citymail.cuny.edu>
2017-03-27 23:29:26 -0400

    Start on simulated annealing

--- ackley.py
+++ ackley.py
@@ -29,7 +29,44 @@ def rand_sample(rand_seed, num_samples):
     return (min_xs, min_y)
 
 
-def hill_climb(rand_seed, step_size, epsilon):
+def best_neighbor(min_xs, min_y, x_idx, step_size):
+    next_xs = np.copy(min_xs)
+    prev_xs = np.copy(min_xs)
+    next_xs[x_idx] += step_size
+    prev_xs[x_idx] -= step_size
+
+    next_y = ackley(next_xs)
+    prev_y = ackley(prev_xs)
+    next_y_diff = min_y - next_y
+    prev_y_diff = min_y - prev_y
+
+    curr_diff = 0
+
+    if prev_y_diff > next_y_diff:
+        if prev_y_diff > 0:
+            min_xs = prev_xs
+            min_y = prev_y
+            curr_diff = prev_y_diff
+    elif prev_y_diff < next_y_diff:
+        if next_y_diff > 0:
+            min_xs = next_xs
+            min_y = next_y
+            curr_diff = next_y_diff
+    else:
+        if prev_y_diff > 0:
+            r = np.random.rand()
+            if r < 0.5:
+                min_xs = prev_xs
+                min_y = prev_y
+                curr_diff = prev_y_diff
+            else:
+                min_xs = next_xs
+                min_y = next_y
+                curr_diff = next_y_diff
+    return (min_xs, min_y, curr_diff)
+
+
+def hill_climb(rand_seed, step_size, epsilon=1e-9):
     d = len(rand_seed)
     min_xs = rand_seed
     min_y = ackley(rand_seed)
@@ -38,40 +75,34 @@ def hill_climb(rand_seed, step_size, epsilon):
         curr_diff = np.inf
         num_iter = 0
         while curr_diff > epsilon and num_iter < 10000:
-            next_xs = np.copy(min_xs)
-            prev_xs = np.copy(min_xs)
-            next_xs[x_idx] += step_size
-            prev_xs[x_idx] -= step_size
-        
-            next_y = ackley(next_xs)
-            prev_y = ackley(prev_xs)
-            next_y_diff = min_y - next_y
-            prev_y_diff = min_y - prev_y
-    
-            if prev_y_diff > next_y_diff:
-                if prev_y_diff > 0:
-                    min_xs = prev_xs
-                    min_y = prev_y
-                    curr_diff = prev_y_diff
-            elif prev_y_diff < next_y_diff:
-                if next_y_diff > 0:
-                    min_xs = next_xs
-                    min_y = next_y
-                    curr_diff = next_y_diff
-            else:
-                if prev_y_diff > 0:
-                    r = np.random.rand()
-                    if r < 0.5:
-                        min_xs = prev_xs
-                        min_y = prev_y
-                        curr_diff = prev_y_diff
-                    else:
-                        min_xs = next_xs
-                        min_y = next_y
-                        curr_diff = next_y_diff
+            min_xs, min_y, curr_diff = best_neighbor(min_xs, min_y, x_idx, step_size)
             num_iter += 1
 
     return (min_xs, min_y)
+
+def temperature(curr, total):
+    return 1 - curr/total
+
+def simulated_annealing(rand_seed, step_size, num_samples, epsilon=1e-9):
+    d = len(rand_seed)
+    min_xs = rand_seed
+    min_y = ackley(rand_seed)
+
+    curr_xs = min_xs
+    curr_y = min_y
+
+    for i in range(num_samples):
+        T = temperature(i, num_samples)
+        rand = np.random.random()
+        if T > rand:
+            curr_x = np.random.rand(d) * (MAX_RANGE - MIN_RANGE) + MIN_RANGE
+            curr_y = ackley(curr_x)
+
+    if curr_y < min_y:
+        min_xs = curr_x
+        min_y = curr_y
+    return (min_xs, min_y)
+
 
 
 if __name__=="__main__":
@@ -80,5 +111,6 @@ if __name__=="__main__":
     print("         Random Sampling (100 samples):", rand_sample(rand_seed, 100))
     print("        Random Sampling (1000 samples):", rand_sample(rand_seed, 1000))
     print("       Random Sampling (10000 samples):", rand_sample(rand_seed, 10000))
-    print("Hill Climbing (0.1 step, 1e-9 epsilon):", hill_climb(rand_seed, step_size=0.1, epsilon=1e-9))
+    print("              Hill Climbing (0.1 step):", hill_climb(rand_seed, step_size=0.1))
+    print("Simulated Annealing (0.1 step, 10000s):", simulated_annealing(rand_seed, step_size=0.1, num_samples=10000))
 
